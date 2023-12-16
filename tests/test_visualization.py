9e7ea60748a9dff926eda66100ddb2052776c0e7
SBS-lchristensen <lchristensen@seabird.com>
2023-12-15 16:55:09 -0800

    fix tests

--- tests/test_visualization.py
+++ tests/test_visualization.py
@@ -12,7 +12,7 @@ import plotly.graph_objects as go
 # Sea-Bird imports
 
 # Internal imports
-from sbs.visualize import visualization as dv
+import sbs.visualize as viz
 
 test_resources = importlib.resources.files('resources')
 
@@ -23,113 +23,113 @@ class TestParseChartData:
         test_resources / "example_pass.asc"
     ])
     def test_parse_instrument_data_csv_pass(self, path):
-        assert isinstance(dv.parse_instrument_data(path), pd.DataFrame)
+        assert isinstance(viz.parse_instrument_data(path), pd.DataFrame)
 
     def test_parse_instrument_data_json_pass(self):
-        assert isinstance(dv.parse_instrument_data(test_resources / "example_pass.json"), pd.DataFrame)
+        assert isinstance(viz.parse_instrument_data(test_resources / "example_pass.json"), pd.DataFrame)
 
     def test_parse_instrument_data_txt_error(self, caplog):
-        dv.parse_instrument_data(test_resources / "example_fail_filetype.txt")
+        viz.parse_instrument_data(test_resources / "example_fail_filetype.txt")
         assert('ERROR' in caplog.text)
 
 
 class TestSelectSubset:
     def test_select_subset_empty_pass(self):
-        data = dv.parse_instrument_data(test_resources / "example_pass.asc")
-        assert np.all(dv.select_subset([], data) == pd.DataFrame({'Sample Count': [0, 1, 2]}))
+        data = viz.parse_instrument_data(test_resources / "example_pass.asc")
+        assert np.all(viz.select_subset([], data) == pd.DataFrame({'Sample Count': [0, 1, 2]}))
 
     def test_select_subset_single_pass(self):
-        data = dv.parse_instrument_data(test_resources / "example_pass.asc")
-        subset = dv.select_subset(['Col1'], data)
+        data = viz.parse_instrument_data(test_resources / "example_pass.asc")
+        subset = viz.select_subset(['Col1'], data)
         assert subset.columns == ['Col1']
         assert np.all(subset == pd.DataFrame({'Col1': [1, 4, 7]}))
 
 
 class TestPlotXYChart:
     data_path = test_resources / 'example_pass.asc'
-    config = dv.ChartConfig(
+    config = viz.ChartConfig(
         title=data_path.name,
         x_names=["Col1"],
         y_names=[],
         z_names=[],
         chart_type=""
     )
-    data = dv.ChartData(data_path, config)
+    data = viz.ChartData(data_path, config)
 
     def test_plot_xy_chart_pass(self):
-        assert isinstance(dv.plot_xy_chart(self.data, self.config), go.Figure)
+        assert isinstance(viz.plot_xy_chart(self.data, self.config), go.Figure)
 
     @pytest.mark.parametrize("chart_type", ["overlay", "subplots"])
     def test_plot_xy_chart_multiple_y_pass(self, chart_type):
         self.config.x_names = []
         self.config.y_names = ["Col1", "Col2", "Col3"]
         self.config.chart_type = chart_type
-        data = dv.ChartData(self.data_path, self.config)
-        assert isinstance(dv.plot_xy_chart(data, self.config), go.Figure)
+        data = viz.ChartData(self.data_path, self.config)
+        assert isinstance(viz.plot_xy_chart(data, self.config), go.Figure)
 
     @pytest.mark.parametrize("chart_type", ["overlay", "subplots"])
     def test_plot_xy_chart_multiple_x_pass(self, chart_type):
         self.config.x_names = ["Col1", "Col2", "Col3"]
         self.config.y_names = []
         self.config.chart_type = chart_type
-        assert isinstance(dv.plot_xy_chart(self.data, self.config), go.Figure)
+        assert isinstance(viz.plot_xy_chart(self.data, self.config), go.Figure)
 
     @pytest.mark.parametrize("chart_type", ["overlay", "subplots"])
     def test_plot_xy_chart_warn(self, chart_type):
         self.config.x_names = ["Col1", "Col2"]
         self.config.y_names = ["Col1", "Col2"]
         self.config.chart_type = chart_type
-        data = dv.ChartData(self.data_path, self.config)
+        data = viz.ChartData(self.data_path, self.config)
         with pytest.warns(UserWarning, match="Only one axis can support multiple data sets"):
-            dv.plot_xy_chart(data, self.config)
+            viz.plot_xy_chart(data, self.config)
 
 
 class TestChartData:
     data_path = test_resources / 'example_pass.asc'
-    config = dv.ChartConfig(
+    config = viz.ChartConfig(
         title=data_path.name,
         x_names=["Col1"],
         y_names=[],
         z_names=[],
         chart_type="overlay"
     )
-    data = dv.ChartData(data_path, config)
+    data = viz.ChartData(data_path, config)
 
     def test_chart_data_x_error(self, caplog):
         with pytest.raises(KeyError):
             config = self.config
             config.x_names = ["Col999"]
-            data = dv.ChartData(self.data_path, config)
+            data = viz.ChartData(self.data_path, config)
 
     def test_chart_data_y_error(self, caplog):
         with pytest.raises(KeyError):
             config = self.config
             config.y_names = ["Col999"]
-            data = dv.ChartData(self.data_path, config)
+            data = viz.ChartData(self.data_path, config)
 
     def test_chart_data_z_error(self, caplog):
         with pytest.raises(KeyError):
             config = self.config
             config.z_names = ["Col999"]
-            data = dv.ChartData(self.data_path, config)
+            data = viz.ChartData(self.data_path, config)
 
     def test_chart_data_slash_pass(self):
         data_path = test_resources / 'example_fail_slash.csv'
-        config = dv.ChartConfig(
+        config = viz.ChartConfig(
             title=data_path,
             x_names = ["Col/1"],
             y_names = ["Col\\2"],
             z_names = ["Col-3"],
             chart_type=""
         )
-        data = dv.ChartData(data_path, config)
+        data = viz.ChartData(data_path, config)
         print(data)
-        assert isinstance(data, dv.ChartData)
+        assert isinstance(data, viz.ChartData)
 
 
 class TestPlotXYChart:
     def test_plot_xy_chart(self):
-        config = dv.ChartConfig(
+        config = viz.ChartConfig(
             title='title',
             x_names=["Col1"],
             y_names=["Col2"],
@@ -137,15 +137,15 @@ class TestPlotXYChart:
             chart_type='subplots'
         )
         source = '{"Col1": [1.0, 4.0, 7.0], "Col2": [2.0, 5.0, 8.0], "Col3": [3.0, 6.0, 9.0], "flag": [0, 0, 0]}'
-        data = dv.ChartData(source, config)
-        figure = dv.plot_xy_chart(data, config)
+        data = viz.ChartData(source, config)
+        figure = viz.plot_xy_chart(data, config)
         assert isinstance(figure, go.Figure)
     # TODO: add failing tests
 
 
 class TestPlotTSChart:
     def test_plot_ts_chart(self):
-        config = dv.ChartConfig(
+        config = viz.ChartConfig(
             title='title',
             x_names=["Col1"],
             y_names=["Col2"],
@@ -153,7 +153,7 @@ class TestPlotTSChart:
             chart_type=''
         )
 
-        figure = dv.plot_ts_chart(
+        figure = viz.plot_ts_chart(
             np.array([1.91865655, 4.61772767, 6.98555581]),
             np.array([1.07317135, 4.21589043, 7.32884086]),
             np.array([1.45629369, 3.65123201, 5.38958412]),
@@ -179,7 +179,7 @@ class TestChartBounds:
     data_path = test_resources / 'example_pass.asc'
     
     def test_chart_bounds_multiple_x_pass(self):
-        config = dv.ChartConfig(
+        config = viz.ChartConfig(
             title=self.data_path.name,
             x_names=["Col1", "Col2", "Col3", "Col4"],
             y_names=[],
@@ -189,11 +189,11 @@ class TestChartBounds:
                 'x': { 0: [1, 5], 1: [6, 8], 2: [5, 9], 3: [9, 11] }
             }
         )
-        data = dv.ChartData(self.data_path, config)
-        assert isinstance(dv.plot_xy_chart(data, config), go.Figure)
+        data = viz.ChartData(self.data_path, config)
+        assert isinstance(viz.plot_xy_chart(data, config), go.Figure)
     
     def test_chart_bounds_multiple_y_pass(self):
-        config = dv.ChartConfig(
+        config = viz.ChartConfig(
             title=self.data_path.name,
             x_names=[],
             y_names=["Col1", "Col2", "Col3", "Col4"],
@@ -203,5 +203,5 @@ class TestChartBounds:
                 'y': { 0: [1, 5], 1: [6, 8], 2: [5, 9], 3: [9, 11] }
             }
         )
-        data = dv.ChartData(self.data_path, config)
-        assert isinstance(dv.plot_xy_chart(data, config), go.Figure)
+        data = viz.ChartData(self.data_path, config)
+        assert isinstance(viz.plot_xy_chart(data, config), go.Figure)
