e7c5c0970e7390e3491a757fd57a91e029af59e0
SBS-lchristensen <108031810+SBS-lchristensen@users.noreply.github.com>
2024-01-16 16:24:35 -0800

    Black and pre-commit (#4)
    * the damage
    
    * adding pre-commit and linted yaml/toml
    
    * add black to dev dependencies, set line-length to 120
    
    * run black
    
    * move viz out of __init__ to improve discoverability
    
    * run black
    
    ---------
    
    Co-authored-by: SBS-jtemplin <jtemplin@seabird.com>

--- src/sbs/process/contour.py
+++ src/sbs/process/contour.py
@@ -21,16 +21,17 @@ class ContourData:
 
     Example variables are shown next to each field
     """
-    x: np.ndarray # absolute salinity
-    y: np.ndarray # conservative temperature
-    z: np.ndarray # potential density
-    x_vec: np.ndarray # absolute salinity vector
-    y_vec: np.ndarray # conservative temperature vector
-    z_mat: np.ndarray # potential density matrix
+
+    x: np.ndarray  # absolute salinity
+    y: np.ndarray  # conservative temperature
+    z: np.ndarray  # potential density
+    x_vec: np.ndarray  # absolute salinity vector
+    y_vec: np.ndarray  # conservative temperature vector
+    z_mat: np.ndarray  # potential density matrix
 
 
 def contour_from_t_s_p(temperature_C, salinity_PSU, pressure_dbar, min_salinity=0, lat=0, lon=0) -> ContourData:
-    """Converts temperature T, salinity S, and pressure P to conservative temperature CT, 
+    """Converts temperature T, salinity S, and pressure P to conservative temperature CT,
     absolute salinity SA, and potential density PD. CT is derived from ITS-90 temperature
     and practical salinity measurements. PD is derived from SA and CT.
 
@@ -46,7 +47,7 @@ def contour_from_t_s_p(temperature_C, salinity_PSU, pressure_dbar, min_salinity=
     """
 
     # Filter out data points where salinity < min_salinity
-    salt_mask = (salinity_PSU > min_salinity)
+    salt_mask = salinity_PSU > min_salinity
     temperature_C = temperature_C[salt_mask]
     salinity_PSU = salinity_PSU[salt_mask]
     pressure_dbar = pressure_dbar[salt_mask]
@@ -54,7 +55,7 @@ def contour_from_t_s_p(temperature_C, salinity_PSU, pressure_dbar, min_salinity=
     # Compute TEOS-10 quantities: SA, CT, potential_density
     absolute_salinity = gsw.SA_from_SP(salinity_PSU, pressure_dbar, lon, lat)
     conservative_temperature = gsw.CT_from_t(absolute_salinity, temperature_C, pressure_dbar)
-    potential_density = gsw.rho(absolute_salinity, conservative_temperature, 0) # TODO: parameterize reference density
+    potential_density = gsw.rho(absolute_salinity, conservative_temperature, 0)  # TODO: parameterize reference density
 
     # Figure out T-S grid boundaries (mins and maxes)
     salt_min = absolute_salinity.min() - (0.01 * absolute_salinity.min())
@@ -63,14 +64,14 @@ def contour_from_t_s_p(temperature_C, salinity_PSU, pressure_dbar, min_salinity=
     temperature_max = conservative_temperature.max() + (0.1 * conservative_temperature.max())
 
     # Calculate how many grid cells we need in the x and y dimensions
-    x_range = round((salt_max - salt_min)/0.1 + 1, 0)
+    x_range = round((salt_max - salt_min) / 0.1 + 1, 0)
     y_range = round((temperature_max - temperature_min) + 1, 0)
     x_cells = x_range.astype(int)
     y_cells = y_range.astype(int)
 
     # Create conservative_temperature and absolute_salinity vectors of appropriate dimensions
-    temperature_vector = np.linspace(1, y_range-1, y_cells) + temperature_min
-    salinity_vector = np.linspace(1, x_range-1, x_cells) * 0.1 + salt_min
+    temperature_vector = np.linspace(1, y_range - 1, y_cells) + temperature_min
+    salinity_vector = np.linspace(1, x_range - 1, x_cells) * 0.1 + salt_min
 
     # Loop to fill in density
     potential_density_matrix = np.zeros((y_cells, x_cells))
@@ -87,7 +88,7 @@ def contour_from_t_s_p(temperature_C, salinity_PSU, pressure_dbar, min_salinity=
         z=potential_density,
         x_vec=salinity_vector,
         y_vec=temperature_vector,
-        z_mat=potential_density_matrix
+        z_mat=potential_density_matrix,
     )
 
     return contour_data
