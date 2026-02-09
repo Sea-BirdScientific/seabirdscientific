17aa91b0673b62e37e837aa33dff71c3e499e73a
SBS-jtemplin <jtemplin@seabird.com>
2026-02-09 15:06:26 -0800

    style: run ruff format

--- docs_src/source/conf.py
+++ docs_src/source/conf.py
@@ -10,29 +10,28 @@ from importlib.metadata import version as get_version
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 
-project = 'Community Toolkit'
-copyright = f'{datetime.now().year}, Sea-Bird Scientific'
-author = 'Sea-Bird Scientific'
-release = get_version('seabirdscientific')
+project = "Community Toolkit"
+copyright = f"{datetime.now().year}, Sea-Bird Scientific"
+author = "Sea-Bird Scientific"
+release = get_version("seabirdscientific")
 version = release
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx_mdinclude',
+    "sphinx.ext.autodoc",
+    "sphinx_mdinclude",
 ]
 
-templates_path = ['_templates']
-html_static_path = ['_static']
+templates_path = ["_templates"]
+html_static_path = ["_static"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_title = f'{project} {version}'
-html_permalinks_icon = '<span>#</span>'
-html_theme = 'sbsfuro'
+html_title = f"{project} {version}"
+html_permalinks_icon = "<span>#</span>"
+html_theme = "sbsfuro"
